c4b0bf52aaeac64cd9c135416f3c7a079b600626
Javier <javierenriquemendozagomez@gmail.com>
2018-07-18 20:00:10 -0500

    Better views for login and register

--- frontend/public/app/AuthenticationModule/AuthenticationCtrl.js
+++ frontend/public/app/AuthenticationModule/AuthenticationCtrl.js
@@ -1,17 +1,48 @@
 "use strict";
 (function () {
-    app.controller('AuthenticationCtrl', function ($scope, AuthenticationService, $rootScope) {
+    app.controller('AuthenticationCtrl', function ($scope, AuthenticationService, $rootScope, $mdToast, $location, $timeout) {
+
         $scope.login = function (user) {
             AuthenticationService.Login(user)
                 .then(function (response) {
                     console.log('Response', response)
-                    if (!response.access_token) {
-
+                    if (response.error) {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent(response.error)
+                                .position('top right')
+                                .hideDelay(5000)
+                        );
                     }
                     else {
                         var loggedUser = response;
                         AuthenticationService.SetCredentials(loggedUser, loggedUser.access_token);
                         $rootScope.user = AuthenticationService.GetCredentials();
+                        $location.url('/posts/')
+                    }
+                })
+        }
+
+        $scope.register = function (user) {
+            AuthenticationService.Register(user)
+                .then(function (response) {
+                    console.log('Response', response)
+                    if (response.error) {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent(response.error)
+                                .position('top right')
+                                .hideDelay(5000)
+                        );
+                    }
+                    else {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent("The user was successfully created!")
+                                .position('top right')
+                                .hideDelay(2000)
+                        );
+                        $timeout(()=>{ $location.url('/auth')}, 2000)
                     }
                 })
         }
